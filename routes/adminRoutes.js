17a047c8660e6ed53ad12cfd0e367a2e7144e95f
abdelboulgha <118727738+abdelboulgha@users.noreply.github.com>
2025-04-06 22:36:31 +0100

    m

--- routes/adminRoutes.js
+++ routes/adminRoutes.js
@@ -6,19 +6,19 @@ const { verifyToken, isSuperAdmin } = require('../middlewares/authMiddleware');
 const citoyenController = require("../controllers/citoyenController");
 
 // Route pour créer un nouvel admin (protégée, seul un superAdmin peut le faire)
-router.post('/admin', verifyToken, isSuperAdmin, adminController.createAdmin);
+router.post('/admin', isSuperAdmin, adminController.createAdmin);
 
 // Route pour obtenir tous les admins (protégée, seul un admin connecté peut le faire)
-router.get('/admins', verifyToken, adminController.getAdmins);
+router.get('/admins', adminController.getAdmins);
 
 // Route pour obtenir un admin par son ID (protégée)
-router.get('/admin/:id', verifyToken, adminController.getAdminById);
+router.get('/admin/:id', adminController.getAdminById);
 
 // Route pour mettre à jour un admin par son ID (protégée)
-router.put('/admin/:id', verifyToken, adminController.updateAdmin);
+router.put('/admin/:id', adminController.updateAdmin);
 
 // Route pour supprimer un admin par son ID (protégée, seul un superAdmin peut le faire)
-router.delete('/admin/:id', verifyToken, isSuperAdmin, adminController.deleteAdmin);
+router.delete('/admin/:id', isSuperAdmin, adminController.deleteAdmin);
 
 router.get("/admins-count", adminController.getAdminsCount);
 
